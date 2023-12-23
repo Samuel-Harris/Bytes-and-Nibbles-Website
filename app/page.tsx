94857239ad877ea88eb10be3516cc4b9b614de63
Samuel Matsuo Harris <samuel.matsuo.harris@gmail.com>
2023-12-23 12:22:08 +0000

    Set up website heading elements and logo

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next"
+import { WEBSITE_NAME } from "./constants/websiteConstants"
+
+export const metadata: Metadata = {
+  title: WEBSITE_NAME,
+  description: "The blog about two of my favourite hobbies: coding and cooking.",
+}
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>Home page</p>
+      </div>
+    </main>
+  )
+}
