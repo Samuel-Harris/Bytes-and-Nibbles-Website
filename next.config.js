b1ba447a0e96c2d0bf03502e586774c8245b2892
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-02-01 23:15:08 +0000

    Disabled linting during building and added names to deployment and preview steps

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
   output: "export",
   distDir: "out",
+  eslint: {
+    ignoreDuringBuilds: true,  // turn linting back on if lint action is disabled
+  },
 };
 
 module.exports = nextConfig;
