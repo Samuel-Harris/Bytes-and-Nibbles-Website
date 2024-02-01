b2c3cd8fa17fc126429a614666206d37d320e14f
Samuel Matsuo Harris <samuel.matsuo.harris@gmail.com>
2024-02-01 23:24:03 +0000

    Add lint GitHub action

--- app/bytes/page.tsx
+++ app/bytes/page.tsx
@@ -7,56 +7,64 @@ import ByteOverview from "../utils/byteOverview";
 import { useImmerReducer } from "use-immer";
 
 type State = {
-    bytes: ByteOverview[];
-}
+  bytes: ByteOverview[];
+};
 
 enum ActionType {
-    UPDATE_BYTE_LIST = "updateByteList"
+  UPDATE_BYTE_LIST = "updateByteList",
 }
 
 type Action = {
-    type: ActionType.UPDATE_BYTE_LIST;
-    bytesList: ByteOverview[];
-}
+  type: ActionType.UPDATE_BYTE_LIST;
+  bytesList: ByteOverview[];
+};
 
 function reducer(draft: State, action: Action): void {
-    switch (action.type) {
-        case ActionType.UPDATE_BYTE_LIST:
-            draft.bytes = action.bytesList;
-            return;
-        default:
-            return;
-    }
+  switch (action.type) {
+    case ActionType.UPDATE_BYTE_LIST:
+      draft.bytes = action.bytesList;
+      return;
+    default:
+      return;
+  }
 }
 
 export default function BytesPage(): JSX.Element {
-    const initState: State = { bytes: [] };
-    const [state, dispatch]: [State, Dispatch<Action>] = useImmerReducer(reducer, initState);
+  const initState: State = { bytes: [] };
+  const [state, dispatch]: [State, Dispatch<Action>] = useImmerReducer(
+    reducer,
+    initState
+  );
 
-    useEffect(() => {
-        let bytesBeenFetched = false;
+  useEffect(() => {
+    let bytesBeenFetched = false;
 
-        if (!bytesBeenFetched) {
-            listBytes().then((bytes: ByteOverview[]) => {
-                dispatch({
-                    type: ActionType.UPDATE_BYTE_LIST,
-                    bytesList: bytes,
-                })
-            });
-        }
+    if (!bytesBeenFetched) {
+      listBytes().then((bytes: ByteOverview[]) => {
+        dispatch({
+          type: ActionType.UPDATE_BYTE_LIST,
+          bytesList: bytes,
+        });
+      });
+    }
 
-        return () => {
-            bytesBeenFetched = true
-        };
-    }, [dispatch])
+    return () => {
+      bytesBeenFetched = true;
+    };
+  }, [dispatch]);
 
-    return (
-        <main className="grid grid-rows-10 justify-items-center pb-6">
-            {state.bytes.map((byteOverview: ByteOverview) =>
-                <div key={byteOverview.title} className="row-span-1 w-full px-24 py-4">
-                    <Tilecard title={byteOverview.title} subtitle={byteOverview.subtitle} thumbnail={byteOverview.thumbnail} publishDate={byteOverview.publishDate} />
-                </div>)
-            }
-        </main>
-    )
+  return (
+    <main className="grid grid-rows-10 justify-items-center pb-6">
+      {state.bytes.map((byteOverview: ByteOverview) => (
+        <div key={byteOverview.title} className="row-span-1 w-full px-24 py-4">
+          <Tilecard
+            title={byteOverview.title}
+            subtitle={byteOverview.subtitle}
+            thumbnail={byteOverview.thumbnail}
+            publishDate={byteOverview.publishDate}
+          />
+        </div>
+      ))}
+    </main>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: "export",
-    distDir: "out",
-  }
+  output: "export",
+  distDir: "out",
+  eslint: {
+    ignoreDuringBuilds: true,  // turn linting back on if lint action is disabled
+  },
+};
 
 module.exports = nextConfig;
