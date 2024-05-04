ec53758960c39ab900d3c19c5c955a4f28a2109e
Samuel Matsuo Harris <samuel.matsuo.harris@gmail.com>
2024-05-04 23:04:46 +0100

    Sort bytes and nibbles by publish date

--- app/common/collectionConstants.ts
+++ app/common/collectionConstants.ts
@@ -1,9 +1,11 @@
 export const bytesCollection = {
   name: "v1_bytes",
   isPublishedField: "isPublished",
+  publishDateField: "publishDate",
 };
 
 export const nibblesCollection = {
   name: "v1_nibbles",
   isPublishedField: "isPublished",
+  publishDateField: "publishDate",
 };
