a618729214611e2abe8c38a472d7d4eb99dbb2c0
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-06-09 19:23:13 +0100

    Added Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import React from "react";
 import { Metadata } from "next";
 import { METADATA_DESCRIPTION_CREDITS, WEBSITE_NAME } from "./common/constants";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { firebaseConfig } from "./common/firebaseConstants";
 
 export const metadata: Metadata = {
   title: WEBSITE_NAME,
@@ -12,11 +14,12 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-};
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
+  <html lang="en">
+    <body>{children}</body>
+    {firebaseConfig.measurementId && (
+      <GoogleAnalytics gaId={firebaseConfig.measurementId} />
+    )}
+  </html>
+);
 export default RootLayout;
