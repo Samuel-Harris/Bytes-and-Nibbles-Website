8536ff7d1631973ba15cce3fc01722afb2762210
Samuel Harris <samuel.matsuo.harris@gmail.com>
2025-05-15 02:48:58 +0100

    Ran prettier again

--- app/nibbles/timeUtils.ts
+++ app/nibbles/timeUtils.ts
@@ -2,5 +2,7 @@ export const getDisplayTime = (totalMinutes: number): string => {
   const hours: number = Math.floor(totalMinutes / 60);
   const minutes: number = totalMinutes % 60;
 
-  return hours > 0 ? `${hours} hours and ${minutes} minutes` : `${minutes} minutes`;
+  return hours > 0
+    ? `${hours} hours and ${minutes} minutes`
+    : `${minutes} minutes`;
 };
