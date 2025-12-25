74031fea7a8ff28589c65b2148abca75faf90827
Samuel Harris <samuel.matsuo.harris@gmail.com>
2025-12-25 19:54:30 +0000

    Fix website lint errors

--- apps/website/__mocks__/react-markdown.js
+++ apps/website/__mocks__/react-markdown.js
@@ -1,11 +1,21 @@
-const React = require('react');
+import React from "react";
 
-const ReactMarkdown = ({ children, remarkPlugins, rehypePlugins, ...otherProps }) => {
+/* eslint-disable react/prop-types */
+const ReactMarkdown = ({
+  children,
+  remarkPlugins: _remarkPlugins,
+  rehypePlugins: _rehypePlugins,
+  ...otherProps
+}) => {
   // Filter out ReactMarkdown-specific props that shouldn't go to DOM
-  return React.createElement('div', {
-    'data-testid': 'react-markdown',
-    ...otherProps
-  }, children);
+  return React.createElement(
+    "div",
+    {
+      "data-testid": "react-markdown",
+      ...otherProps,
+    },
+    children
+  );
 };
 
 module.exports = ReactMarkdown;
